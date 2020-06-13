11d08e49de435e498a4f2dcc3a5c7c357b7059a7
Suleman2708 <64948507+Suleman2708@users.noreply.github.com>
2020-06-13 11:29:36 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+// Chapter # 1 
+
+// alert ("Welcome to Suleman Website");
+
+// alert ("Error! Please enter a valid password");
+
+// alert ("Welcome to Javascript Land Happy Coding");
+
+// lert ("Welcome to Javascript Land");
+// alert ("Prevent this page from creating additional dialogues");
+
+// console.log ("Hello....I can run JS through my web browsers console");
+
+
+// Chapter # 2
+
+// alert("User name");
+
+// var username ="Suleman Imtiaz";
+// alert (username);
+
+// var message="Hello world";
+// alert (message);
+
+
+// var name="Suleman Imtiaz"
+// alert(name)
+// var age ="22 years old"
+// alert (age)
+// var work ="Certified Mobile Application Development"
+// alert (work)
+
+
+
+// Chapter 3
+
+
+// var age="I am 22 Years old"
+// alert (age);
+
+
+// var a=1; 
+// var b=a++; 
+// alert(b); 
+
+
+// var birthyear= "My Birth Year is 1998"; 
+// document.write(birthyear);
+
+
+// Chapter 5 
+
+// var a=5; 
+// var b=10; 
+// var c= a+b; 
+// document.write(c);
+
+
+
+// var a=15; 
+// var b=10; 
+// var c= a-b; 
+// document.write(c);
+
